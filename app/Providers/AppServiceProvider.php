05a9239f05c3fec85355acacdcc23d9b0800f321
Tiffany <tiffanymyap06@gmail.com>
2026-04-30 15:12:26 +0800

    4/30/26

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,18 +1,53 @@
 <?php
 
+
+
 namespace App\Providers;
+
+
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL;
+
+
+
 class AppServiceProvider extends ServiceProvider
+
 {
-    //register
+
+    /**
+
+     * Register any application services.
+
+     */
+
     public function register(): void
+
     {
+
+        //
+
     }
 
-    //bootstrap
+
+
+    /**
+
+     * Bootstrap any application services.
+
+     */
+
     public function boot(): void
+
     {
+
+        if (env('APP_ENV') === 'production') {
+
+            URL::forceScheme('https');
+
+        }
+
     }
+
 }
